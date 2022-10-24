a40aee6e1c6b8ae4c08136ca92da9eb3549fe922
Davy-k <davy.kambrun2021@campus-eni.fr>
2022-10-24 16:04:44 +0200

    add NavBar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TPSortieFront';
+
+  navLinks = [
+    {
+      label : "Gros caca",
+      link:"profile"
+    },
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { ConnexionComponent } from './connexion/connexion.component';
+import {RouterModule} from "@angular/router";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnexionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    RouterModule.forRoot([
+      { path: '', component: ConnexionComponent}
+    ]),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

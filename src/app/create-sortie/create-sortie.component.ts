f3fd09edaef39a8773dee6c6bd7d352856cf4aa8
juliendeux <deuxjulien@gmail.com>
2022-10-25 11:57:13 +0200

    Suite Create série

---
+++ src/app/create-sortie/create-sortie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-sortie',
+  templateUrl: './create-sortie.component.html',
+  styleUrls: ['./create-sortie.component.css']
+})
+export class CreateSortieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
